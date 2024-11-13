6f388f6d0e24afc4c50192a994bc206e54372054
wzy <2049562063@qq.com>
2024-11-13 17:41:34 +0800

    set default screen is login

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -38,7 +35,7 @@ fun MainView() {
     val navController = rememberNavController()
     NavHost(
         navController = navController,
-        startDestination = "content",
+        startDestination = "login",
     ) {
         composable("login") {
             Login(navController)
