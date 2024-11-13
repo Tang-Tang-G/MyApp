15190149f657eb0d2867344cafd434aef3dd1599
wzy <2049562063@qq.com>
2024-11-13 17:16:12 +0800

    refactor

--- app/src/main/java/com/example/myapp/compose/BottomBar.kt
+++ app/src/main/java/com/example/myapp/compose/BottomBar.kt
@@ -1,53 +1,33 @@
 package com.example.myapp.compose
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Build
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.MutableIntState
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.navigation.NavController
-
-// TODO:
-class Navigation(val label: String, val navigate: String)
+import com.example.myapp.model.Navigation
 
 
 @Composable
-fun BottomBar(navController: NavController) {
-    var selectIndex by remember { mutableIntStateOf(0) }
-
-    val items = listOf("首页", "家庭", "场景", "我的")
-    val inner = listOf("overview", "home", "scene", "my")
-    val icons = listOf(
-        Icons.Filled.Home,
-        Icons.Filled.Favorite,
-        Icons.Filled.Build,
-        Icons.Filled.AccountCircle
-    )
-
-    // TODO:
-    val navigation = items.zip(inner).map { (l, n) -> Navigation(l, n) }
-
+fun BottomBar(
+    navController: NavController,
+    navigation: List<Navigation>,
+    selectIndex: MutableIntState,
+) {
     BottomAppBar {
         NavigationBar {
-            items.forEachIndexed { index, item ->
+            navigation.forEachIndexed { index, navigation ->
                 NavigationBarItem(
-                    icon = { Icon(icons[index], contentDescription = item) },
-                    label = { Text(item) },
-                    selected = selectIndex == index,
+                    icon = navigation.icon,
+                    label = { Text(navigation.label) },
+                    selected = selectIndex.intValue == index,
                     onClick = {
-                        selectIndex = index
-                        navController.navigate(inner[index])
-                    },
+                        selectIndex.intValue = index
+                        navController.navigate(navigation.navigate)
+                    }
                 )
             }
         }
