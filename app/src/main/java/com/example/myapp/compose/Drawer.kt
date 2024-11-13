c5d49f084e60350b383aa94b4274c58790b34760
wzy <2049562063@qq.com>
2024-11-13 17:18:29 +0800

    add drawer

--- app/src/main/java/com/example/myapp/compose/Drawer.kt
+++ app/src/main/java/com/example/myapp/compose/Drawer.kt
@@ -1,9 +1,27 @@
 package com.example.myapp.compose
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 
+@Preview(showBackground = true)
 @Composable
 fun DrawerContent() {
-    Text("Side Bar")
+    Surface(
+        modifier = Modifier.fillMaxHeight().fillMaxWidth(0.7f),
+        color = Color.White
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Text("Side Bar")
+        }
+    }
 }
