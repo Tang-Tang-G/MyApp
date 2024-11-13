e9632d77e0c9048ae3725f073d825fd440a30a68
wzy <2049562063@qq.com>
2024-11-13 17:18:46 +0800

    init model

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.myapp.model.InitModel
 import com.example.myapp.screens.Content
 import com.example.myapp.screens.Login
 import com.example.myapp.ui.theme.MyAppTheme
@@ -23,6 +24,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            InitModel()
             MyAppTheme {
                 MainView()
             }
