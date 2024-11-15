508456d8390ffc855a8e75b573671281c6c1c6f8
wzy <2049562063@qq.com>
2024-11-15 13:41:27 +0800

    use callback to replace navController

--- app/src/main/java/com/example/myapp/screens/LoginScreen.kt
+++ app/src/main/java/com/example/myapp/screens/LoginScreen.kt
@@ -1,8 +1,12 @@
 package com.example.myapp.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
@@ -15,12 +19,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.navigation.NavController
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapp.ui.theme.MyAppTheme
 
 
 @Composable
-fun Login(navController: NavController) {
+fun Login(
+    navigateToContent: () -> Unit = {},
+    navigateToSignup: () -> Unit = {},
+    navigateToForgetPassword: () -> Unit = {}
+) {
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     Surface(
@@ -55,27 +66,35 @@ fun Login(navController: NavController) {
                 singleLine = true,
                 visualTransformation = PasswordVisualTransformation()
             )
+            Spacer(modifier = Modifier.fillMaxHeight(0.5f))
             Row(
                 horizontalArrangement = Arrangement.SpaceEvenly,
                 verticalAlignment = Alignment.CenterVertically,
             ) {
-                Button(
-                    onClick = {
-                        navController.navigate("content") {
-                            popUpTo("login") { inclusive = true }
-                        }
-                    }
-                ) {
+                Button(onClick = navigateToContent) {
                     Text("login") // TODO: add to strings.xml, replaced by stringResource
                 }
-                Button(
-                    onClick = {
-                        navController.navigate("content")
-                    }
-                ) {
-                    Text("login") // TODO: add to strings.xml, replaced by stringResource
+                Button(onClick = navigateToSignup) {
+                    Text("signup") // TODO: add to strings.xml, replaced by stringResource
                 }
             }
+            Text(
+                text = "forget password?",
+                modifier = Modifier
+                    .clickable {
+                        navigateToForgetPassword()
+                    },
+                color = Color.Gray,
+                textDecoration = TextDecoration.Underline
+            )
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginPreview() {
+    MyAppTheme {
+        Login()
     }
 }
