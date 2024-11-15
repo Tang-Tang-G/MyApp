69ca04844237d640e9df952dce2fa70372b78df7
wzy <2049562063@qq.com>
2024-11-15 13:41:49 +0800

    add some string value

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -4,9 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -33,32 +32,27 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainView() {
     val navController = rememberNavController()
+    val loginNav = stringResource(R.string.screen_nav_login_navigation)
+    val contentNav = stringResource(R.string.screen_nav_content_navigation)
+
     NavHost(
         navController = navController,
-        startDestination = "login",
+        startDestination = loginNav,
     ) {
-        composable("login") {
-            Login(navController)
+        composable(loginNav) {
+            Login(navigateToContent = {
+                navController.navigate(contentNav) {
+                    popUpTo(loginNav) {
+                        inclusive = true
+                        saveState = true
+                    }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            })
         }
-        composable("content") {
+        composable(contentNav) {
             Content(navController)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyAppTheme {
-        Greeting("A")
     }
 }
