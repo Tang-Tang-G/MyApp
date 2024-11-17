8808cfcd8138a338fd00a4d411aa24d4a9eaf4c2
wzy <2049562063@qq.com>
2024-11-17 11:47:25 +0800

    replace nav controller with go-back callback

--- app/src/main/java/com/example/myapp/compose/TopBar.kt
+++ app/src/main/java/com/example/myapp/compose/TopBar.kt
@@ -1,8 +1,6 @@
 package com.example.myapp.compose
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.Menu
@@ -11,35 +9,27 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.Modifier.Companion
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.navigation.NavController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopBarWithBackArrow(navController: NavController, title:String) {
+fun TopBarWithBack(title: String, goBack: () -> Unit) {
     TopAppBar(
         title = { Text(title) },
         navigationIcon = {
-            IconButton(onClick = {
-                // 点击图标时，返回到上一个页面
-                navController.popBackStack()
-            }) {
+            IconButton(onClick = goBack) {
                 Icon(
                     imageVector = Icons.Filled.KeyboardArrowLeft,
                     contentDescription = "Close",
-                    tint =Color.Black
+                    tint = Color.Black
                 )
             }
         },
     )
 }
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
