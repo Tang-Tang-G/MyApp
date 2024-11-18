2cada79d3b59f29b3ae4af0f169d973a3a184268
TH <3176089369@qq.com>
2024-11-18 19:42:46 +0800

    添加 JsonParser.kt Area.kt Device.kt House.kt

---
+++ app/src/main/java/com/example/myapp/database/Device.kt
@@ -0,0 +1,7 @@
+package com.example.myapp.database
+
+data class Device(
+    val deviceID: Int,
+    val name: String,
+    val type: String
+)
---
+++ app/src/main/java/com/example/myapp/network/JsonParser.kt
@@ -0,0 +1,52 @@
+package com.example.myapp.network
+
+import android.util.Log
+import com.example.myapp.database.Area
+import com.example.myapp.database.Device
+import com.example.myapp.database.House
+import org.json.JSONException
+import org.json.JSONObject
+
+class JsonParser {
+
+    fun parseJsonData(data: JSONObject): List<House> {
+        val houseList = mutableListOf<House>()
+        try {
+            val housesDevices = data.getJSONArray("houses_devices")
+            for (i in 0 until housesDevices.length()) {
+                val house = housesDevices.getJSONObject(i)
+                val houseInfo = house.getJSONObject("house_info")
+                val houseName = houseInfo.getString("house_name")
+                Log.e("HouseName", "houseName: $houseName")
+
+                val newHouse = House(houseName)  // 创建新房屋对象
+                val areasDevices = house.getJSONArray("areas_devices")
+                for (j in 0 until areasDevices.length()) {
+                    val area = areasDevices.getJSONObject(j)
+                    val areaInfo = area.getJSONObject("area_info")
+                    val areaName = areaInfo.getString("area_name")
+
+                    val newArea = Area(areaName)  // 创建新区域对象
+
+                    val devices = area.getJSONArray("devices")
+                    for (k in 0 until devices.length()) {
+                        val device = devices.getJSONObject(k)
+                        val deviceName = device.getString("device_name")
+                        val deviceType = device.getJSONObject("device_type").getString("type_name")
+                        val deviceID = device.getInt("device_id")
+                        // 创建设备对象并添加到区域
+                        val deviceObj = Device(deviceID, deviceName, deviceType)
+                        newArea.addDevice(deviceObj)
+                    }
+                    // 将区域添加到房屋
+                    newHouse.addArea(newArea)
+                }
+                // 将房屋添加到房屋列表
+                houseList.add(newHouse)
+            }
+        } catch (e: JSONException) {
+            Log.e("JsonParser", "JSON parsing error: ${e.message}")
+        }
+        return houseList
+    }
+}
