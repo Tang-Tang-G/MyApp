8416d12f96a24ef4a1dbab7b3960665d21a22012
wzy <2049562063@qq.com>
2024-11-18 10:49:20 +0800

    update view

--- app/src/main/java/com/example/myapp/screens/login/LoginScreen.kt
+++ app/src/main/java/com/example/myapp/screens/login/LoginScreen.kt
@@ -1,13 +1,21 @@
 package com.example.myapp.screens.login
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -16,14 +24,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapp.compose.TopBar
+import com.example.myapp.network.login
 import com.example.myapp.ui.theme.MyAppTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun Login(
@@ -33,6 +45,13 @@ fun Login(
 ) {
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var checked by remember { mutableStateOf(false) }
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+//
+//    val loginViewModel = LoginViewModel()
+//    val loginResult = loginViewModel.loginResult.observe()
+
     Surface(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -44,7 +63,11 @@ fun Login(
             modifier = Modifier.fillMaxSize()
         ) {
             TopBar(title = { Text(text = "登录") })
-
+            Icon(
+                imageVector = Icons.Default.Face,
+                contentDescription = "Account",
+                modifier = Modifier.fillMaxSize(0.2f)
+            )
             Spacer(modifier = Modifier.height(50.dp))
             TextField(
                 value = username,
@@ -52,7 +75,7 @@ fun Login(
                     username = value
                 },
                 label = {
-                    Text("username") // TODO: add to strings.xml, replaced by stringResource
+                    Text("用户名") // TODO: add to strings.xml, replaced by stringResource
                 },
                 singleLine = true,
             )
@@ -63,27 +86,64 @@ fun Login(
                     password = value
                 },
                 label = {
-                    Text("password") // TODO: add to strings.xml, replaced by stringResource
+                    Text("输入密码") // TODO: add to strings.xml, replaced by stringResource
                 },
                 singleLine = true,
                 visualTransformation = PasswordVisualTransformation(),
             )
             Spacer(modifier = Modifier.height(50.dp))
+            Button(
+                onClick = {
+                    if (checked) {
+                        scope.launch {
+                            val response = login("wzy", "123456")
+                            Log.d(this.javaClass.name, response.orEmpty())
+                        }
+                        navigateToContent()
+                    } else {
+                        scope.launch {
+                            snackbarHostState.showSnackbar("需要确定协议!")
+                        }
+                    }
+                },
+                modifier = Modifier.fillMaxWidth(0.6f)
+            ) {
+                Text("登录") // TODO: add to strings.xml, replaced by stringResource
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.height(20.dp)
+            ) {
+                Checkbox(
+                    checked = checked,
+                    onCheckedChange = {
+                        checked = !checked
+                    },
+                )
+                Text(
+                    text = "我已阅读并同意服务协议",
+                    fontSize = 14.sp
+                )
+            }
             Row(
                 horizontalArrangement = Arrangement.SpaceEvenly,
-                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .padding(top = 10.dp)
             ) {
-                Button(onClick = navigateToContent) {
-                    Text("Sign in") // TODO: add to strings.xml, replaced by stringResource
+                TextButton(onClick = navigateToSignup) {
+                    Text("注册用户") // TODO: add to strings.xml, replaced by stringResource
                 }
-                Spacer(modifier = Modifier.width(50.dp))
-                Button(onClick = navigateToSignup) {
-                    Text("Sign up") // TODO: add to strings.xml, replaced by stringResource
+                TextButton(onClick = navigateToForgetPassword) {
+                    Text("忘记密码")
                 }
             }
-            TextButton(onClick = navigateToForgetPassword) {
-                Text("forget password?")
-            }
+
+            Spacer(modifier = Modifier.height(50.dp))
+            SnackbarHost(
+                hostState = snackbarHostState,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
         }
     }
 }
